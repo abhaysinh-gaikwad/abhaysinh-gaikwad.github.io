4f40c1713819326fd8dc21f3206b1245668e10d7
Abhay <abhaygaikwad7666@gmail.com>
2025-07-23 12:20:19 +0530

    Add sitemap and robots.txt via next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://portfolio.abhaysinh.dev", 
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  generateIndexSitemap: false
+};
